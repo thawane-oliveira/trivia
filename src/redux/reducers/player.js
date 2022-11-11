ee53df1be046a16851cdc3efaca5d366de72c12f
Pietro Bergamaschi <pietrovb25@gmail.com>
2022-11-10 21:58:01 -0300

    Requisito 9 concluído

--- src/redux/reducers/player.js
+++ src/redux/reducers/player.js
@@ -1,22 +1,17 @@
 import { SUBMIT_INFO, SCORE_POINTS } from '../actions';
 
 const INITIAL_STATE = {
-  player: {
-    name: '',
-    email: '',
-    score: 0,
-    assertions: 0,
-  },
+  player: {},
 };
 
 const player = (state = INITIAL_STATE, action) => {
   switch (action.type) {
   case SUBMIT_INFO:
     return {
-      ...state,
       name: action.payload.name,
       email: action.payload.email,
       score: 0,
+      assertions: 0,
     };
   case SCORE_POINTS:
     return {

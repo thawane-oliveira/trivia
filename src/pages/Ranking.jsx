6d1ea2f1cadbcc4e56dda244eef6f892b0e009f9
antonio <antonio.nogueiraneto@icloud.com>
2022-11-11 16:26:43 -0300

    adicionando requisito 18

--- src/pages/Ranking.jsx
+++ src/pages/Ranking.jsx
@@ -1,18 +1,32 @@
+import { func } from 'prop-types';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import BtnHome from '../components/BtnHome';
 
 class Ranking extends Component {
+  handleHome = () => {
+    const { history } = this.props;
+    history.push('/');
+  };
+
   render() {
     return (
       <div>
 
         <h1 data-testid="ranking-title">Ranking</h1>
-        <BtnHome />
+        <button
+          type="button"
+          data-testid="btn-go-home"
+          onClick={ this.handleHome }
+        >
+          home
+        </button>
       </div>
 
     );
   }
 }
+Ranking.propTypes = {
+  history: func,
+}.isRequired;
 
 export default connect()(Ranking);
